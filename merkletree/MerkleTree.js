85dab495d2da69562df4ac53449239f186773c53
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2025-01-02 17:08:56 +0900

    Create MerkleTree.js

---
+++ merkletree/MerkleTree.js
@@ -0,0 +1,18 @@
+export default class MerkleTree {
+  /*
+  TODO: 다음의 조건을 만족하는 생성자를 만들어주세요.
+  - 생성자의 첫번째 인자는 리프 노드들로 구성된 배열을 받습니다. 
+  - 생성자의 두번째 인자는 두 노드를 결합하고 해시하는 함수를 받습니다.
+  - root 속성에 트리의 루트 노드를 할당해주세요.
+  */
+  constructor(leaves, concat) {
+    this.root = concat(leaves[0], leaves[1]);
+  }
+  /*
+  TODO: 다음의 조건을 만족하는 함수를 만들어주세요.
+  - 트리의 루트 노드를 찾아주는 함수입니다.
+  */
+  getRoot() {
+    return this.root;
+  }
+}
